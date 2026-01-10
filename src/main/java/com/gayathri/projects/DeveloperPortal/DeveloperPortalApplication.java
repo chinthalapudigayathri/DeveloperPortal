d50df16606043bc13be6c38b678be9ce7a16f849
chinthalapudigayathri <gayathri.ch.sde@gmail.com>
2026-01-10 11:15:07 +0530

    Initial commit

---
+++ src/main/java/com/gayathri/projects/DeveloperPortal/DeveloperPortalApplication.java
@@ -0,0 +1,13 @@
+package com.gayathri.projects.DeveloperPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperPortalApplication.class, args);
+	}
+
+}
